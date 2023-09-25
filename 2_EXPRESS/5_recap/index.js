af433a1bc8a82ce9f637723d187f0e356d809dc5
EA - Otávio Henrique Moraes Cruz <cruz_otavio@educadventista.org>
2023-09-25 14:49:45 -0300

    recapitulação de render html

--- 2_EXPRESS/5_recap/index.js
+++ 2_EXPRESS/5_recap/index.js
@@ -1,13 +1,16 @@
 const express = require("express")
+const path = require("path")
 
 const app = express()
 
+const caminho = path.join(__dirname, "templates")
+
 app.get('/', (requisicao, resposta) => {
-    reposta.send("Seja bem-vindo(a) ao meu servidor!")
+    reposta.sendFile(`${caminho}/index.html`)
 })
 
 app.listen(3000, () => {
-    console.log("Servidor rodando na porta 3001")
+    console.log("Servidor rodando na porta 3000")
 })
 
 // http://localhost:3000
